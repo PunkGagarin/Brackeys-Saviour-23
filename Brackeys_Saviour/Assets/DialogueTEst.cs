3e285caf800f8501abcdcee7172254713705f759
Roman <41580710+PunkGagarin@users.noreply.github.com>
2023-02-18 05:09:14 +0300

    Ink import

---
+++ Brackeys_Saviour/Assets/DialogueTEst.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DialogueTEst : MonoBehaviour {
+
+    // [SerializeField]
+    // private DialogueContainer _dialogueContainer;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
